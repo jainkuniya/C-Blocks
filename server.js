33721b0c89b93476d2a07b71f7fff78d004d3204
Aditya Jain <aditya.jain@zomato.com>
2018-03-17 13:57:47 +0530

    Update : Server.js

---
+++ server.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('Welcome to C-Blocks');
+});
+
+app.listen(3000, function () {
+  console.log('Server is up and running on port 3000');
+});
