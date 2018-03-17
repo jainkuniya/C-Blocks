a85bebbfb304888ad99fb5adc2870b722195c29b
Aditya Jain <aditya.jain@zomato.com>
2018-03-17 17:16:18 +0530

    Update : Connect projects front end

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 var express = require('express');
 var app = express();
 
+var contractorRouter = require('./routes/contractor');
+
 app.get('/', function (req, res) {
   res.send('Welcome to C-Blocks');
 });
 
+app.use("/contractor",contractorRouter);
+
 app.listen(3000, function () {
   console.log('Server is up and running on port 3000');
 });
+
+module.exports = app;
